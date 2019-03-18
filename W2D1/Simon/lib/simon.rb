b4872cfb3507caaf89e03e1dc429e1310f82181b
your name here <laikarenh@gmail.com>
2019-03-18 00:28:46 -0700

    Add loop in #require_sequence

--- W2D1/Simon/lib/simon.rb
+++ W2D1/Simon/lib/simon.rb
@@ -19,10 +19,9 @@ class Simon
   end
 
   def take_turn
-    if @game_over == true
-      show_sequence
-      require_sequence
-    else 
+    show_sequence
+    require_sequence
+    unless @game_over
       round_success_message
       @sequence_length += 1
     end
@@ -30,12 +29,22 @@ class Simon
 
   def show_sequence
     add_random_color
+    #loop through @seq, for ea color, puts color
+    #sleep for one second (sleep 0.75)
+    #clear system
+    #sleep for another half second
   end
   
-  def require_sequence #working on this spec
+  def require_sequence
     puts "Repeat a color: "
-    color_input = gets.chomp.downcase
-    @game_over = true if !seq.include?(color_input)
+    @seq.each do |color|
+      color_input = gets.chomp.downcase
+      if color != color_input
+        @game_over = true
+        break
+      end
+    end
+ #sleep 0.25
   end
 
   def add_random_color
@@ -43,11 +52,11 @@ class Simon
   end
 
   def round_success_message
-    puts "You've passed this round!"
+    puts "You've passed this round! Get ready for the next..."
   end
 
   def game_over_message
-    puts "Game over!"
+    puts "Game over! You've passed #{@sequence_length-1} rounds."
   end
 
   def reset_game
