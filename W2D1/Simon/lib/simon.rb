298860a28c345a2a4e9f248daa29803067377e97
your name here <laikarenh@gmail.com>
2019-03-18 00:14:36 -0700

    Create Simon Class

---
+++ W2D1/Simon/lib/simon.rb
@@ -0,0 +1,60 @@
+class Simon
+  COLORS = %w(red blue green yellow)
+
+  attr_accessor :sequence_length, :game_over, :seq
+
+  def initialize(sequence_length=1, seq=[])
+    @sequence_length = sequence_length
+    @game_over = false
+    @seq = seq
+  end
+
+  def play
+    take_turn until game_over == true
+    game_over_message
+    reset_game
+  end
+
+  def take_turn
+    if @game_over == true
+      show_sequence
+      require_sequence
+    else 
+      round_success_message
+      @sequence_length += 1
+    end
+  end
+
+  def show_sequence
+    add_random_color
+  end
+  
+  def require_sequence #working on this spec
+    puts "Repeat a color: "
+    color_input = gets.chomp.downcase
+
+    if !seq.include?(color_input)
+      @game_over = true
+    else 
+      @game_over = false
+    end
+  end
+
+  def add_random_color
+    @seq << COLORS.sample
+  end
+
+  def round_success_message
+    puts "You've passed this round!"
+  end
+
+  def game_over_message
+    puts "Game over!"
+  end
+
+  def reset_game
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
+  end
+end
