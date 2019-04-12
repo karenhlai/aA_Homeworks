e6defbf01d921a4a0e9703ca6097877c2b3f57c7
your name here <karenlai@Karens-MacBook-Pro.local>
2019-04-12 00:58:29 -0700

    Complete teaAndBiscuits

--- W5D5/teaAndBiscuits.js
+++ W5D5/teaAndBiscuits.js
@@ -5,21 +5,18 @@ const reader = readline.createInterface({
     output: process.stdout
 });
 
-function teaAndBiscuits() {
-    let first, second;
 
+function teaAndBicsuits() {
     reader.question('Would you like some tea?', (res) => {
-        first = res;
         console.log(`You replied ${res}.`);
     });
-
+    
     reader.question('Would you like some biscuits?', (res) => {
-        second = res;
         console.log(`You replied ${res}.`);
     });
 
-    const firstRes = (first === 'yes') ? 'do' : 'don\'t';
-    const secondRes = (second === 'yes') ? 'do' : 'don\'t';
+    const first = (res === 'yes') ? 'do' : 'don\'t'; 
+    const second = (res2 === 'yes') ? 'do' : 'don\'t'; 
     console.log(`So you ${firstRes} want tea and you ${secondRes} want biscuits.`);
     reader.close();
-}
+};
