865ba48f6484c54e99b526a120169ba3a334cc98
your name here <karenlai@Karens-MacBook-Pro.local>
2019-04-12 00:49:39 -0700

    Complete setTimeOut funcs

---
+++ W5D5/callbacksAndFuncCalls.js
@@ -0,0 +1,11 @@
+// asynchronous 
+window.setTimeout(function () {
+    alert('HAMMERTIME');
+}, 5000);
+
+//timeout plus closure - 
+function hammerTime(time) {
+    window.setTimeout(function () {
+        alert('${ time } is hammertime!');
+    }, time);
+};
